03cb9e12aba50d152ec87508daea78e58e817f16
Aaron Black <black27@llnl.gov>
2019-05-01 15:13:07 -0700

    Add code example showing inability to use debugger tools with FORTRAN OpenMP code from XLF.

---
+++ testing/tools/unable_to_debug_fortran_omp/saxpy_omp45_f.F90
@@ -0,0 +1,28 @@
+subroutine testsaxpy_omp45_f
+
+  implicit none
+  integer, parameter :: N = ishft(1,21)
+  integer :: i
+! segfaults, out of memory stack issue?
+!  real :: x(N), y(N), a
+ 
+  real, allocatable :: x(:), y(:)
+  real :: a
+
+  allocate( x(N), y(N) )
+  x = 1.0
+  y = 2.0
+  a = 2.0
+
+  !$omp target data map(to:N,a,x) map(tofrom: y)
+
+  !$omp target teams distribute parallel do private(i) shared(y,a,x) default(none)
+  do i=1,N
+    y(i) = a*x(i) + y(i)
+  end do
+  !$omp end target teams distribute parallel do
+
+  !$omp end target data
+
+  write(*,*) "Ran FORTRAN OMP45 kernel. Max error: ", maxval(abs(y-4.0))
+end subroutine testsaxpy_omp45_f
