b4be5301d83e0ffa624356351aad30abbde8816a
Aaron Black <black27@llnl.gov>
2020-02-20 09:28:56 -0800

    Correct the cuda vs openmp loop test location to the performance directory.

--- examples/interoperability/cuda_omp45_coexisting/main.cc
+++ examples/interoperability/cuda_omp45_coexisting/main.cc
@@ -2,19 +2,22 @@
 #include "omp.h"
 #include "kernels.h"
 
+extern "C" void __xlcuf_init(void);
+
 int main(int argc, char *argv[])
 {
 
-   int numThreads = omp_get_max_threads();
+   __xlcuf_init();
 
+   int numThreads = omp_get_max_threads();
    std::cout << "Num max threads: " << numThreads << std::endl;
 
    #pragma omp parallel for
    for (int i = 0; i < numThreads; ++i)
    {
-      testSaxpy_cudac();
-//      testsaxpy_cudafortran();
-      testsaxpy_omp45();
+   	testSaxpy_cudac();
+	   testsaxpy_cudafortran();
+   	testsaxpy_omp45();
    }
 	return (0);
 }
