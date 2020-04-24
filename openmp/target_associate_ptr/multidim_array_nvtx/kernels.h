0bf46c2487775a99c0be51e0bdfbdeaddeb967fe
Aaron Black <black27@llnl.gov>
2020-04-23 17:06:46 -0700

    Add OpenMP example showing overhead of using FORTRAN arrays on the GPU.

---
+++ openmp/target_associate_ptr/multidim_array_nvtx/kernels.h
@@ -0,0 +1,6 @@
+// In saxpy.F90
+extern "C" void testsaxpy_omp45_f_();
+
+// In saxpy.c
+void testsaxpy_omp45_c(void);
+
---
+++ openmp/target_associate_ptr/multidim_array_nvtx/main.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "omp.h"
+#include "kernels.h"
+
+int main(int argc, char *argv[])
+{
+
+   int nThreads = 1;
+   int iters = 10;
+
+   std::cout << "Num threads: " << nThreads << std::endl;
+
+   #pragma omp parallel for num_threads(nThreads)
+   for (int i = 0; i < nThreads; ++i)
+   {
+		for (int j = 0; j < iters; ++j)
+      {
+		  testsaxpy_omp45_c();
+        testsaxpy_omp45_f_();
+	   }
+   }
+
+	return (0);
+}
