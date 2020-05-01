57c6f809ec50e2cac1f8014bea7bda9923e32845
Aaron Black <black27@llnl.gov>
2020-04-30 19:31:24 -0700

    Added memory measurements around kernels.

--- cuda_openmp_interoperability/cuda_omp45_coexisting/main.cc
+++ cuda_openmp_interoperability/cuda_omp45_coexisting/main.cc
@@ -1,11 +1,18 @@
+#include <cstdio>
 #include <iostream>
 #include "omp.h"
 #include "kernels.h"
+#include "cuda_runtime_api.h"
 
 extern "C" void __xlcuf_init(void);
 
 int main(int argc, char *argv[])
 {
+   size_t free = 0, total = 0;                                                                                                                                                                                     
+   cudaError_t status;                                                                                                                                                                                             
+   
+   status = cudaMemGetInfo(&free, &total);
+   printf("Before xlcuf_init call: GPU's memory: %.2f MB used, %.2f MB free.\n", (double)(total-free)/1048576.0, (double)free/1048576.0);
 
    __xlcuf_init();
 
@@ -15,9 +22,19 @@ int main(int argc, char *argv[])
    #pragma omp parallel for
    for (int i = 0; i < numThreads; ++i)
    {
+
    	testSaxpy_cudac();
+   	status = cudaMemGetInfo(&free, &total);
+   	printf("After CUDA C kernel run: GPU's memory: %.2f MB used, %.2f MB free.\n", (double)(total-free)/1048576.0, (double)free/1048576.0);
+
 	   testsaxpy_cudafortran();
+   	status = cudaMemGetInfo(&free, &total);
+   	printf("After CUDA FORTRAN kernel run: GPU's memory: %.2f MB used, %.2f MB free.\n", (double)(total-free)/1048576.0, (double)free/1048576.0);
+
    	testsaxpy_omp45();
+   	status = cudaMemGetInfo(&free, &total);
+   	printf("After OpenMP C kernel: %.2f MB used, %.2f MB free.\n", (double)(total-free)/1048576.0, (double)free/1048576.0);
+
    }
 	return (0);
 }
