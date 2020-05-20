09ac1c321795e10315c2720eed385e915b05047f
Aaron Black <black27@llnl.gov>
2020-05-20 15:39:01 -0700

    remove old compiler testing across clang, nvcc, xlf.  These cases are deprecated.

--- testing/compiler_interoperability_test_cases/xl_clang_omp_test/kernels.h
+++
@@ -1,6 +0,0 @@
-// In saxpy.F90
-extern "C" void testsaxpy_omp45_f_();
-
-// In saxpy.c
-void testsaxpy_omp45_c(void);
-
--- testing/compiler_interoperability_test_cases/xl_nvcc_test/test_cudafortran.f90
+++
@@ -1,6 +0,0 @@
-subroutine fsubroutine()
-
-  write (*,*) "Hello from FORTRAN"
-  call test_saxpy_cuda_kernels
-
-end
